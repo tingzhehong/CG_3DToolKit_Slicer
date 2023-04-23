543b78f4f049ca1e52edab8903cf8c58112e5e29
tingzhehong <891166296@qq.com>
2023-04-23 14:15:56 +0800

    third commit

--- CG_3DToolKit_Slicer/src/view/CGDataTreeView.cpp
+++ CG_3DToolKit_Slicer/src/view/CGDataTreeView.cpp
@@ -47,16 +47,38 @@ void CGDataTreeView::InitUi()
     m_ToolBoxTopItem->addChild(m_3DToolBox);
     m_ToolBoxTopItem->addChild(m_ProfileToolBox);
 
+
+    m_NodeFlowTopItem = new QTreeWidgetItem(QStringList{tr(u8"流程算子")});
+
+    m_Maths = new QTreeWidgetItem(QStringList{tr(u8"数学计算")});
+    for (int j = 0; j < m_MathsNames.size(); ++j) {
+        QTreeWidgetItem *pMathsItem = new QTreeWidgetItem(QStringList() << m_MathsNames.at(j));
+        m_Maths->addChild(pMathsItem);
+    }
+
+    m_Logics = new QTreeWidgetItem(QStringList{tr(u8"逻辑条件")});
+    for (int j = 0; j < m_LogicsNames.size(); ++j) {
+        QTreeWidgetItem *pLogicsItem = new QTreeWidgetItem(QStringList() << m_LogicsNames.at(j));
+        m_Logics->addChild(pLogicsItem);
+    }
+
+    m_NodeFlowTopItem->addChild(m_Maths);
+    m_NodeFlowTopItem->addChild(m_Logics);
+
+
     m_FuctionTopItem = new QTreeWidgetItem(QStringList{tr(u8"功能算子")});
-    m_2DFuction = new QTreeWidgetItem(QStringList{tr(u8"2D算子")});
-    m_3DFuction = new QTreeWidgetItem(QStringList{tr(u8"3D算子")});
+    m_2DFuction = new QTreeWidgetItem(QStringList{tr(u8"2D功能算子")});
+    m_3DFuction = new QTreeWidgetItem(QStringList{tr(u8"3D功能算子")});
 
     m_FuctionTopItem->addChild(m_2DFuction);
     m_FuctionTopItem->addChild(m_3DFuction);
 
+
     m_DataTree->addTopLevelItem(m_ToolBoxTopItem);
+    m_DataTree->addTopLevelItem(m_NodeFlowTopItem);
     m_DataTree->addTopLevelItem(m_FuctionTopItem);
     m_DataTree->expandItem(m_ToolBoxTopItem);
+    m_DataTree->expandItem(m_NodeFlowTopItem);
     m_DataTree->expandItem(m_FuctionTopItem);
 
     QVBoxLayout *pTreeLayout = new QVBoxLayout();
--- CG_3DToolKit_Slicer/src/view/CGDataTreeView.h
+++ CG_3DToolKit_Slicer/src/view/CGDataTreeView.h
@@ -21,16 +21,23 @@ public:
     QTreeWidget *m_DataTree;
 
     QTreeWidgetItem *m_ToolBoxTopItem ;
+    QTreeWidgetItem *m_NodeFlowTopItem;
     QTreeWidgetItem *m_FuctionTopItem;
 
     QTreeWidgetItem *m_2DToolBox;
     QTreeWidgetItem *m_3DToolBox;
     QTreeWidgetItem *m_ProfileToolBox;
+
+    QTreeWidgetItem *m_Maths;
+    QTreeWidgetItem *m_Logics;
+
     QTreeWidgetItem *m_2DFuction;
     QTreeWidgetItem *m_3DFuction;
 
     QStringList m_2DToolNames{u8"线段", u8"矩形", u8"圆", u8"圆弧"};
     QStringList m_3DToolNames{u8"距离", u8"角度", u8"包围盒"};
+    QStringList m_MathsNames{u8"加", u8"减", u8"乘", u8"除"};
+    QStringList m_LogicsNames{u8"条件", u8"循环"};
     QStringList m_ProfileToolNames{u8"2点指定", u8"垂直线", u8"水平线", u8"矩形", u8"圆", u8"圆弧"};
 
 };
