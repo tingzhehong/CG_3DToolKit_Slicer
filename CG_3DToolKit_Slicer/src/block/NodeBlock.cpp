dd0ea5a091f8d4c8fcc14ef11535761de65bc96f
tingzhehong <891166296@qq.com>
2023-06-28 15:44:25 +0800

    Update

--- CG_3DToolKit_Slicer/src/block/NodeBlock.cpp
+++ CG_3DToolKit_Slicer/src/block/NodeBlock.cpp
@@ -67,7 +67,7 @@ NodeItem *NodeBlock::NodeItemNumberInput(QString nodename)
     m_NodeItem->setPos(x, y);
 
     PortItem *portOut = m_NodeItem->createPortOut(8, QColor(Qt::cyan));
-    connect(lineEdit, &QLineEdit::textChanged, this, [=](QString str){ portOut->setValue(QVariant::fromValue(str.trimmed().toFloat())); });
+    connect(lineEdit, &QLineEdit::textChanged, this, [=](QString str){ portOut->setValue(QVariant::fromValue(str.trimmed().toFloat())); m_IsRuned = true; });
 
     return m_NodeItem;
 }
